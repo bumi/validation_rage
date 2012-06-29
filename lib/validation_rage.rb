a555916c4da9a55944fad6bacc7f8e4ce30abd2c
Michael Bumann <michael@railslove.com>
2012-06-30 00:40:33 +0200

    oops ;)

--- lib/validation_rage.rb
+++ lib/validation_rage.rb
@@ -4,6 +4,6 @@ require 'validation_rage/rails' if defined?(Rails)
 module ValidationRage
   autoload :BaseNotifier, "validation_rage/base_notifier"
   autoload :LogNotifier, "validation_rage/log_notifier"
-  autoload :FnordMetricNotifier, "validation_rage/log_notifier"
+  autoload :FnordMetricNotifier, "validation_rage/fnord_metric_notifier"
   autoload :ModelExtension, "validation_rage/model_extension"
 end
