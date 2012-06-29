a00c9f7267f7eb12a6e0c224ccb565586a4f5764
Michael Bumann <michael@railslove.com>
2012-06-30 01:25:48 +0200

    rename initializer

--- lib/validation_rage/rails.rb
+++ lib/validation_rage/rails.rb
@@ -1,14 +1,14 @@
 module ValidationRage
   class Engine < ::Rails::Engine
-    
+
     config.validation_rage = ActiveSupport::OrderedOptions.new
 
     config.validation_rage.attach_to = [] 
     config.validation_rage.attach_to = [ActiveRecord::Base] if defined?(ActiveRecord::Base) # really?!
-    
-    config.validation_rage.notifier = {"Log" => {:log_level => :warn, :logger => nil}}
 
-    initializer "validation_rage.configure_validation_rage_subscriber" do |app|
+    config.validation_rage.notifier = {"Log" => {:log_level => :warn, :logger => nil}} # defalt log everything to the logger
+
+    initializer "validation_rage.configure_subscribers" do |app|
 
       config.validation_rage.notifier.each do |name, options|
         options[:logger] ||= Rails.logger
@@ -17,8 +17,7 @@ module ValidationRage
       app.config.validation_rage.attach_to.each do |klass|
         klass.send(:include, ValidationRage::ModelExtension)
       end
-      #ActiveRecord::Base.descendants.each {|m| m.send(:include, ValidationRage::ModelExtension) }
-      #ActiveRecord::Base.send(:include, ValidationRage::ModelExtension)
+
     end
   end
 end
