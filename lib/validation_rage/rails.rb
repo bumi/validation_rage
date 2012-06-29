90a4963e4249dca06f24cb54303ea681330140ab
Michael Bumann <michael@railslove.com>
2012-06-30 01:49:23 +0200

    allow to easily disable validation_rage

--- lib/validation_rage/rails.rb
+++ lib/validation_rage/rails.rb
@@ -9,15 +9,16 @@ module ValidationRage
     config.validation_rage.notifier = {"Log" => {:log_level => :warn, :logger => nil}} # defalt log everything to the logger
 
     initializer "validation_rage.configure_subscribers" do |app|
-
-      config.validation_rage.notifier.each do |name, options|
-        options[:logger] ||= Rails.logger
-        ValidationRage.const_get("#{name}Notifier").new(options).subscribe!
+      # TODO move this into a testable setup method
+      if app.config.validation_rage
+        app.config.validation_rage.notifier.each do |name, options|
+          options[:logger] ||= Rails.logger
+          ValidationRage.const_get("#{name}Notifier").new(options).subscribe!
+        end
+        app.config.validation_rage.attach_to.each do |klass|
+          klass.send(:include, ValidationRage::ModelExtension)
+        end
       end
-      app.config.validation_rage.attach_to.each do |klass|
-        klass.send(:include, ValidationRage::ModelExtension)
-      end
-
     end
   end
 end
