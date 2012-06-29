dcc73f66762d7fd796141a63a4c106e74db85de6
Michael Bumann <michael@railslove.com>
2012-06-30 01:21:22 +0200

    support multiple subscribers

--- lib/validation_rage/rails.rb
+++ lib/validation_rage/rails.rb
@@ -6,15 +6,14 @@ module ValidationRage
     config.validation_rage.attach_to = [] 
     config.validation_rage.attach_to = [ActiveRecord::Base] if defined?(ActiveRecord::Base) # really?!
     
-    config.validation_rage.notifier_options            = ActiveSupport::OrderedOptions.new
-    config.validation_rage.notifier_options.logger     = nil # seems we do not have access to the default logger here. so we set the default in the initializer
-    config.validation_rage.notifier_options.log_level  = :warn
-    config.validation_rage.notifier                    = "Log"
+    config.validation_rage.notifier = {"Log" => {:log_level => :warn, :logger => nil}}
 
     initializer "validation_rage.configure_validation_rage_subscriber" do |app|
-      app.config.validation_rage.notifier_options.logger ||= Rails.logger
-      
-      ValidationRage.const_get("#{app.config.validation_rage.notifier}Notifier").new(app.config.validation_rage.notifier_options).subscribe!
+
+      config.validation_rage.notifier.each do |name, options|
+        options[:logger] ||= Rails.logger
+        ValidationRage.const_get("#{name}Notifier").new(options).subscribe!
+      end
       app.config.validation_rage.attach_to.each do |klass|
         klass.send(:include, ValidationRage::ModelExtension)
       end
