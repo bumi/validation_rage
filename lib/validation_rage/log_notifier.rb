509672082066f6a973dd4d85cd907452cd7b0d3e
Michael Bumann <michael@railslove.com>
2012-06-29 23:55:09 +0200

    do not log empty error hashes

--- lib/validation_rage/log_notifier.rb
+++ lib/validation_rage/log_notifier.rb
@@ -6,6 +6,7 @@ module ValidationRage
       self.log_level  = args[:log_level]  || :warn
     end
     def call(event_name, payload)
+      return if payload.values.first && payload.values.first.empty?
       self.logger.send(self.log_level, "#{event_name} #{payload}")
     end
   end
--- test/validation_rage/log_notifier_test.rb
+++ test/validation_rage/log_notifier_test.rb
@@ -7,9 +7,17 @@ class ValidationRage::LogNotifierTest < MiniTest::Unit::TestCase
     assert_equal :warn, ValidationRage::LogNotifier.new({}).log_level
   end
   def test_log_validation_errors_to_logger
+    payload = {"Class" => {:name => ["missing"]}}
     mocked_logger = mock()
-    mocked_logger.expects(:info).with("event_name payload")
+    mocked_logger.expects(:info).with("event_name #{payload.inspect}")
     log_notifier = ValidationRage::LogNotifier.new(:logger => mocked_logger, :log_level => :info)
-    log_notifier.call("event_name", "payload")
+    log_notifier.call("event_name", payload)
+  end
+
+  def test_ignore_empty_errors
+    mocked_logger = mock()
+    mocked_logger.expects(:warn).never
+    notifier = ValidationRage::LogNotifier.new(:logger => mocked_logger)
+    notifier.call("event_name", "Class" => {})
   end
 end
