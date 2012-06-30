7cb89cc776dddaab32ac3fc7e39c376d9d256d56
Michael Bumann <michael@railslove.com>
2012-06-30 03:02:11 +0200

    do not open the socket connection on initialize

--- lib/validation_rage/udp_notifier.rb
+++ lib/validation_rage/udp_notifier.rb
@@ -2,14 +2,15 @@ require "socket"
 module ValidationRage
   class UdpNotifier < BaseNotifier
 
-    attr_accessor :socket
+    attr_accessor :socket, :host, :port
     def initialize(args)
       self.socket = UDPSocket.new
-      self.socket.connect(args[:host], args[:port])
+      self.host = args[:host]
+      self.port = args[:port]
     end
 
     def call(event_name, payload)
-      self.socket.send(payload.to_json, 0)
+      self.socket.send(payload.to_json, 0, self.host, self.port)
     end
   end
 
